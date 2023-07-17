668f67e0eeba144bd9544be6776deef031c6746e
Marek <56512676+jzmark@users.noreply.github.com>
2023-07-17 13:45:06 +0100

    Uploading locally stored files to GitHub

---
+++ src/main/java/com/coursework/Main.java
@@ -0,0 +1,84 @@
+package com.coursework;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ResourceConnector connector = new ResourceConnector();
+        System.out.println("*** Add cities ***");
+        //CheckStyle was complaining about founding dates being magic numbers, but I don't really see the point
+        //of creating separate int variables for each one unless I'm wrong
+        connector.addCity(new City("Italy", "Marche", "Ancona", 387));
+        connector.addCity(new City("Italy", "Lazio", "Rome", -753));
+        connector.addCity(new City("Greece",  "Attica", "Athens", -3000));
+        connector.addCity(new City("Greece", "Attica", "Pireaus", -2600));
+        connector.addCity(new City("Greece",  "Messinia", "Messini", 1867));
+        connector.addCity(new City("Greece", "Messinia", "Pylos", 1833));
+
+        System.out.println("\n*** Add Rome again - should return 409 ***");
+        connector.addCity(new City("Italy", "Lazio", "Rome", -753));
+
+        System.out.println("\n*** Output all current cities ***");
+        //all arguments set to null as no filter equals get all cities
+        get(connector, null, null, null);
+
+        System.out.println("\n*** Delete Ancona and then print the cities again ***");
+        connector.delete("Italy", "Marche", "Ancona");
+        get(connector, null, null, null);
+
+        System.out.println("\n*** Try to delete Ancona again - should return 404 ***");
+        connector.delete("Italy", "Marche", "Ancona");
+
+        System.out.println("\n*** Try to delete a made up city called Neverland in a "
+                + "\nreal state and country (Lazio, Italy) - should return 404 ***");
+        connector.delete("Italy", "Lazio", "Neverland");
+
+        System.out.println("\n**** Get all cities founded before 1700 ***");
+        get(connector, null, null, 1700);
+
+        System.out.println("\n*** Get all cities in Italy - should only be Rome as Ancona was deleted ***");
+        get(connector, "Italy", null, null);
+
+        System.out.println("\n*** Get all cities in Greece - should be Athens, Pireaus, Messini and Pylos ***");
+        get(connector, "Greece", null, null);
+
+        System.out.println("\n*** Get all cities in Messinia in Greece - should be Pylos and Messini ***");
+        get(connector, "Greece", "Messinia", null);
+
+        System.out.println("\n*** Create and add a (made up) city called Athens founded year 1 "
+                + "\nin a province called Attica in Italy - should return 200 ***");
+        connector.addCity(new City("Italy", "Attica", "Athens", 1));
+
+        System.out.println("\n*** Get all cities in Italy having added new fake one ***");
+        get(connector, "Italy", null, null);
+
+        System.out.println("\n*** Return all cities older than 0 in Italy - NOT explicitly "
+                + "\nasked for, should only be Rome ...***");
+        get(connector, "Italy", null, 0);
+    }
+
+    /**
+     * Get method moved to main to show that retrieved
+     * Cities get correctly converted back into java objects
+     * from passed JSON
+     * @param connector connector used to connect to the service
+     * @param country country name
+     * @param state state name
+     * @param founded date of foundation
+     */
+    public static void get(ResourceConnector connector, String country, String state, Integer founded) {
+        String foundedString = null;
+        if (founded != null) {
+            //converting from Integer to String to keep data persistent
+            foundedString = Integer.toString(founded);
+        }
+        ArrayList<City> cities = connector.get(country, state, foundedString);
+        if (cities != null) {
+            //outputting all retrieved City objects
+            for (City value : cities) {
+                System.out.printf("%s was founded in %d and is in %s, which is in %s%n",
+                        value.getCity(), value.getFoundingDate(), value.getState(), value.getCountry());
+            }
+        }
+    }
+}
---
+++ src/main/java/com/coursework/ResourceConnector.java
@@ -0,0 +1,102 @@
+package com.coursework;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+/**
+ * Resource connector used to connect to the server and retrieve date
+ * @author Marek Jezinski - Based on Neal Harman's lab 14 Resource Connector
+ */
+public class ResourceConnector {
+
+    private final Client client = Client.create();
+    private final WebResource baseURI = client.resource("http://localhost:8080/webapi/webresource");
+
+    /**
+     * Adding a city
+     * @param newCity city object
+     */
+    public void addCity(City newCity) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            ClientResponse response = baseURI.path("add").type(MediaType.APPLICATION_JSON)
+                    .post(ClientResponse.class, mapper.writeValueAsString(newCity));
+            System.out.printf("Adding: %s was founded in %d "
+                            + "and is in %s, which is in %s -  Response code: %d%n",
+                    newCity.getCity(), newCity.getFoundingDate(),
+                    newCity.getState(), newCity.getCountry(), response.getStatus());
+        } catch (IOException e) {
+            System.out.println("Conversion to JSON failed");
+        }
+    }
+
+    /**
+     * Get request from the server to get cities only satisfying relevant filters
+     * @param country country name filter
+     * @param state state name filter
+     * @param founded foundation date filter
+     * @return array list of City objects satisfying requirements set
+     */
+    public ArrayList<City> get(String country, String state, String founded) {
+        //MultivaluedMap is necessary for queryParams method implementation
+        MultivaluedMap<String, String> queryParamMap = new MultivaluedMapImpl();
+        //adding relevant parameters if they are set
+        if (country != null) {
+            queryParamMap.add("country", country);
+        }
+        if (state != null) {
+            queryParamMap.add("state", state);
+        }
+        if (founded != null) {
+            queryParamMap.add("founded", founded);
+        }
+        //retrieving cities from server with relevant filters
+        ClientResponse response = baseURI.path("get").queryParams(queryParamMap).type(MediaType.APPLICATION_JSON)
+                .get(ClientResponse.class);
+        //same here with checkstyle complaining about magic number
+        if (response.getStatus() != 200) {
+            System.out.println("GET Failed with code: " + response.getStatus());
+            return null;
+        }
+        //converting retrieved cities to arraylist of City objects
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayList<City> cities = null;
+        try {
+            cities = mapper.readValue(response.getEntity(String.class),
+                    new TypeReference<ArrayList<City>>() {
+                    });
+        } catch (IOException e) {
+            System.out.println("City not found");
+        }
+        return cities;
+    }
+
+    /**
+     * Deleting City set with parameters
+     * @param country country name
+     * @param state state name
+     * @param city city name
+     */
+    public void delete(String country, String state, String city) {
+        MultivaluedMap<String, String> queryParamMap = new MultivaluedMapImpl();
+        //all parameters must be set otherwise server will not return anything
+        queryParamMap.add("country", country);
+        queryParamMap.add("state", state);
+        queryParamMap.add("city", city);
+
+        WebResource webTarget = baseURI.path("delete").queryParams(queryParamMap);
+        ClientResponse response = webTarget.type(MediaType.APPLICATION_JSON).delete(ClientResponse.class);
+        System.out.println(response.getEntity(String.class) + " -  Response code: " + response.getStatus());
+    }
+}
+
